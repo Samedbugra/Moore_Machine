57e69a2946d16d4de1a52f959ce92874a5dd4730
Samedbugra <76523944+Samedbugra@users.noreply.github.com>
2021-09-08 22:54:21 +0300

    Add files via upload

---
+++ MooreMachine/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MooreMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tboxR = new System.Windows.Forms.TextBox();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.tboxQ = new System.Windows.Forms.TextBox();
+            this.tboxE = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 48F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(143, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(516, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moore Makinası";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(346, 148);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(66, 27);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(298, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Durumlar kümesinin eleman sayısı (q0,q1...)";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(229, 219);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Q {q0,q1...}  = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(247, 268);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "∑ {a,b...}  = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(247, 314);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 16);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Γ {0,1...}  = ";
+            // 
+            // tboxR
+            // 
+            this.tboxR.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxR.Location = new System.Drawing.Point(336, 311);
+            this.tboxR.Margin = new System.Windows.Forms.Padding(2);
+            this.tboxR.Name = "tboxR";
+            this.tboxR.Size = new System.Drawing.Size(192, 23);
+            this.tboxR.TabIndex = 8;
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonGo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonGo.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(162)));
+            this.buttonGo.Location = new System.Drawing.Point(512, 395);
+            this.buttonGo.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(231, 44);
+            this.buttonGo.TabIndex = 9;
+            this.buttonGo.Text = "Tablo Olustur";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // tboxQ
+            // 
+            this.tboxQ.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxQ.Location = new System.Drawing.Point(336, 216);
+            this.tboxQ.Margin = new System.Windows.Forms.Padding(2);
+            this.tboxQ.Name = "tboxQ";
+            this.tboxQ.ReadOnly = true;
+            this.tboxQ.Size = new System.Drawing.Size(192, 23);
+            this.tboxQ.TabIndex = 10;
+            // 
+            // tboxE
+            // 
+            this.tboxE.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tboxE.Location = new System.Drawing.Point(336, 265);
+            this.tboxE.Margin = new System.Windows.Forms.Padding(2);
+            this.tboxE.Name = "tboxE";
+            this.tboxE.Size = new System.Drawing.Size(192, 23);
+            this.tboxE.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(770, 473);
+            this.Controls.Add(this.tboxE);
+            this.Controls.Add(this.tboxQ);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.tboxR);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Moore Makinesi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tboxR;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox tboxQ;
+        private System.Windows.Forms.MaskedTextBox tboxE;
+    }
+}
+
---
+++ MooreMachine/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Author Samed Buğra Karataş
+
+namespace MooreMachine
+{
+    public partial class Form1 : Form
+    {
+        public int qSayisi;
+        public String strQ,strE,strR;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            tboxQ.Text = null;
+            qSayisi = comboBox1.SelectedIndex;
+            
+            for(int i=1;i<=qSayisi;i++)
+            {
+                tboxQ.Text +="q"+(i-1);
+                if (i != qSayisi)
+                    tboxQ.Text += ',';
+            } 
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)
+        { 
+            strQ = tboxQ.Text.ToString();
+            for(int i=0;i< tboxR.Text.ToString().Length;i++)
+            {
+                if(Char.IsDigit(tboxR.Text.ToString()[i]))
+                    strR += tboxR.Text.ToString()[i];
+            }
+            for (int i = 0; i < tboxE.Text.ToString().Length; i++)
+            {
+                if(Char.IsLetter(tboxE.Text.ToString()[i]))
+                    strE += tboxE.Text.ToString()[i];
+            }
+
+             
+            if (strQ=="" || strE==null || strR==null || qSayisi<strE.Length)// q sayisi E sayısından fazla olması gerekir
+            {
+                MessageBox.Show("Girdileri Kontrol Ediniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                strE = null; strQ = null;
+            }
+            else
+            {
+                Table table = new Table();
+                table.Tablo_Olustur(qSayisi, strE, strR);
+                table.Show();
+                
+                this.Hide();
+            } 
+        }
+    }
+}
